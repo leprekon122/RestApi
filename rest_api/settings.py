c7704fcca0d375c475e0003925b75014352c0b69
priest <ami8800@gmail.com>
2022-04-19 16:28:47 +0300

    new6

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.path.join("rest_api/key.py")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rest-api111.herokuapp.com']
 
 # Application definition
 
