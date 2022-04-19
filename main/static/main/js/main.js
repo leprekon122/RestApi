6a56bba48e6be8c19fcffd963a7c010250c09c12
priest <ami8800@gmail.com>
2022-04-19 16:58:38 +0300

    new8

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -41,7 +41,7 @@ function chose_color_out(){
 
 function get_url(){
     var current_url = document.URL
-    if(current_url == 'http://rest-api111.herokuapp.com'){
+    if(current_url != 'https://rest-api111.herokuapp.com/'){
         document.getElementById('create_news').innerHTML='Create News';
 
     }
