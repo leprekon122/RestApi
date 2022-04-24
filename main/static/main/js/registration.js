fe71acca81d7471fedcc5ee8f55428e777c09c0a
priest <ami8800@gmail.com>
2022-04-24 18:55:19 +0300

    login9

--- main/static/main/js/registration.js
+++ main/static/main/js/registration.js
@@ -1,8 +1,7 @@
-function login_off(){
-    var current_url = document.URL
-    console.log(current_url)
-    if(current_url == 'https://rest-api111.herokuapp.com/registration/'){
+function log_btn(){
+    var users = document.getElementById('username').innerHTML
+    if(users != 'AnonymousUser'){
         document.getElementById('login_nav').style.display='none';
     }
 }
-login_off()
+log_btn()
