8fcff50f17f570d477ce835d95e9fa467e72dfa2
priest <ami8800@gmail.com>
2022-04-24 19:03:56 +0300

    login11

--- main/static/main/js/registration.js
+++ main/static/main/js/registration.js
@@ -1,7 +1,8 @@
 function log_btn(){
-    var users = document.getElementById('username').innerHTML
-    if(users != 'AnonymousUser'){
-        document.getElementById('login_nav').style.display='none';
+    var current_url = document.URL
+    if(current_url != 'https://rest-api111.herokuapp.com/'){
+    document.getElementById('login_nav').style.display='none';
     }
 }
+
 log_btn()
